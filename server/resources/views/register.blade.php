9e1104c1d659a13363bc3ef22707af34f54b3b5b
ImLauraGS <lgilsolano@gmail.com>
2024-03-27 14:18:30 +0100

    mail works on register

---
+++ server/resources/views/register.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registro exitoso</title>
+</head>
+<body>
+    <h2>¡Hola {{ $user->name }}!</h2>
+    <p>Gracias por registrarte en nuestra aplicación.</p>
+</body>
+</html>
