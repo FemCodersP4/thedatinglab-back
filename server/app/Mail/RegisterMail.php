bbb59a6006ee3b2d9f907420f2ad7c24af43a11a
stef-ania <stebofof@gmail.com>
2024-03-27 10:17:05 +0100

    Added user instance in registerMail

--- server/app/Mail/RegisterMail.php
+++ server/app/Mail/RegisterMail.php
@@ -5,14 +5,21 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
+//use Illuminate\Mail\Mailables\Content;
+//use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 use App\Models\User;
 
 class RegisterMail extends Mailable
 {
     use Queueable, SerializesModels;
+
+    /**
+     * The user instance.
+     *
+     * @var \App\Models\User
+     */
+    public $user;
 
     /**
      * Create a new message instance.
@@ -31,5 +38,5 @@ class RegisterMail extends Mailable
          return $this->view('emails.register')
                      ->subject('Bienvenido a DatingLab');
      }
-   
+
 }
