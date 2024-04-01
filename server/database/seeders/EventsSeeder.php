dc87d328214254a94b7c4c7811f325a80baf1ba7
ImLauraGS <lgilsolano@gmail.com>
2024-04-01 22:09:08 +0200

    hotfix seeder events images

--- server/database/seeders/EventsSeeder.php
+++ server/database/seeders/EventsSeeder.php
@@ -18,7 +18,7 @@ class EventsSeeder extends Seeder
                 'date' => '2024-04-23',
                 'time' => '18:00:00',
                 'description' => 'Magnifica ocasion para cata de vinos',
-                'image' => 'upload/test1.jpg',
+                'image' => 'uploads/test1.jpg',
                 'user_id' => '1',
             ],
             [
@@ -26,7 +26,7 @@ class EventsSeeder extends Seeder
                 'date' => '2024-06-02',
                 'time' => '10:00:00',
                 'description' => 'Descubre las últimas obras de artistas contemporáneos emergentes en esta exposición imperdible.',
-                'image' => 'upload/test2.jpg',
+                'image' => 'uploads/test2.jpg',
                 'user_id' => '1',
             ],
             [
@@ -34,7 +34,7 @@ class EventsSeeder extends Seeder
                 'date' => '2024-07-08',
                 'time' => '14:00:00',
                 'description' => 'Aprende a cocinar platos auténticos de Tailandia de la mano de chefs expertos.',
-                'image' => 'upload/test2.jpg',
+                'image' => 'uploads/test2.jpg',
                 'user_id' => '1',
             ],
             [
@@ -42,7 +42,7 @@ class EventsSeeder extends Seeder
                 'date' => '2024-08-20',
                 'time' => '11:00:00',
                 'description' => 'Explora una colección única de libros antiguos y raros en esta feria literaria especializada.',
-                'image' => 'upload/test4.jpg',
+                'image' => 'uploads/test4.jpg',
                 'user_id' => '1',
             ],
             [
@@ -50,10 +50,10 @@ class EventsSeeder extends Seeder
                 'date' => '2024-09-17',
                 'time' => '17:00:00',
                 'description' => 'Prueba una amplia variedad de delicias culinarias de todo el mundo en este festival gastronómico multicultural.',
-                'image' => 'upload/test5.jpg',
+                'image' => 'uploads/test5.jpg',
                 'user_id' => '1',
             ],
-            
+
         ];
             DB::table('events')->insert($events);
     }
