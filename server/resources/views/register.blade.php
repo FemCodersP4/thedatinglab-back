7766e0a537d98d423a72c5d9bf468f9b19e61eb1
Jasz-17 <jaszapata1983@gmail.com>
2024-04-02 09:54:55 +0200

    test email template

--- server/resources/views/register.blade.php
+++ server/resources/views/register.blade.php
@@ -3,18 +3,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Confirmación de Registro</title>
+    <title>Email Template</title>
     <style>
-        @import url('https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css');
+    
     </style>
 </head>
-<body class="bg-gray-100">
-    <div class="max-w-md mx-auto p-8 bg-white rounded-md shadow-md">
-        <h1 class="text-2xl font-bold text-pink-600 mb-4">¡Bienvenido a nuestro sitio!</h1>
-        <p class="text-gray-700 mb-4">¡Gracias por registrarte! Estamos emocionados de tenerte con nosotros.</p>
-        <p class="text-gray-700 mb-4">Para comenzar a disfrutar de todos nuestros eventos, simplemente inicia sesión con tu cuenta recién creada.</p>
-        <p class="text-gray-700 mb-6">¡Esperamos verte pronto en nuestro sitio!</p>
-        <a href="{{ $loginUrl }}" class="inline-block px-6 py-3 bg-pink-600 text-white rounded-md hover:bg-pink-700 transition duration-300">Iniciar Sesión</a>
-    </div>
+<body class="bg-pink-100">
+
+    
+    <header class="bg-red-500 py-4 text-white text-center">
+        <img src="ruta/a/logo.png" alt="Logo" class="mx-auto" style="max-width: 200px;">
+    </header>
+
+    <main class="p-4">
+        <h1 class="text-2xl text-center mb-4">¡Hola [Nombre]!</h1>
+        <p class="text-center mb-6">Gracias por unirte a nuestra comunidad.</p>
+        <p class="text-center mb-6">Tu participación es fundamental para hacer de [Nombre de la App] el mejor lugar para encontrar conexiones significativas.</p>
+        <p class="text-center mb-6">¿Podrías completar tu registro y rellenar nuestra breve encuesta? Tu opinión nos ayuda a mejorar.</p>
+        <div class="text-center">
+            <a href="#" class="bg-red-500 text-white px-4 py-2 rounded inline-block">Completar Encuesta</a>
+        </div>
+    </main>
+
+    
+    <footer class="bg-white py-4 text-center">
+        <div>
+            <img src="ruta/a/logo.png" alt="Logo" class="mx-auto mb-4" style="max-width: 150px;">
+        </div>
+        <div class="mb-2">
+            <p class="text-gray-500">Contáctanos:</p>
+            <p>Correo electrónico: contacto@ejemplo.com</p>
+            <p>Teléfono: +123456789</p>
+        </div>
+        <div class="mb-2">
+            <p class="text-gray-500">Síguenos en:</p>
+            <a href="#" class="text-red-500 mx-2">Facebook</a>
+            <a href="#" class="text-red-500 mx-2">Twitter</a>
+            <a href="#" class="text-red-500 mx-2">Instagram</a>
+        </div>
+        <p class="text-gray-500">Este correo fue enviado desde [Nombre de la App].</p>
+    </footer>
+
 </body>
 </html>
