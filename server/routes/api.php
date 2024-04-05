15ce775660e47a79db493294cdfd19c062bed43c
ImLauraGS <lgilsolano@gmail.com>
2024-04-05 10:24:28 +0200

    hotfix: changed id on router api

--- server/routes/api.php
+++ server/routes/api.php
@@ -56,6 +56,6 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/event/attendance/{id}', [AttendancesController::class, 'confirmAttendance']);
     Route::get('/event/attendance/{id}', [AttendancesController::class, 'eventAttendees']);
     Route::get('/event/user/{id}', [AttendancesController::class, 'getEventsForUser']);
-    Route::get('/event/{event}', [EventController::class, 'show']);
+    Route::get('/event/{id}', [EventController::class, 'show']);
     Route::delete('/user/{id}', [UserController::class, 'destroy']);
 });
