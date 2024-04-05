a5d2c7580d7671e019da9023c6eedf829096e52b
ImLauraGS <lgilsolano@gmail.com>
2024-04-05 09:21:05 +0200

    events show passed to private

--- server/routes/api.php
+++ server/routes/api.php
@@ -42,7 +42,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/event', [EventController::class, 'index']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/event/{event}', [EventController::class, 'show']);
+
 
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
@@ -55,5 +55,6 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/event/attendance/{id}', [AttendancesController::class, 'confirmAttendance']);
     Route::get('/event/attendance/{id}', [AttendancesController::class, 'eventAttendees']);
     Route::get('/event/user/{id}', [AttendancesController::class, 'getEventsForUser']);
+    Route::get('/event/{event}', [EventController::class, 'show']);
 
 });
