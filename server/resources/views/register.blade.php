6a29cbc6df3e63d9e9acd5c40aeda1a7788316c9
debora-smb <debora.smborges@gmail.com>
2024-04-10 12:49:45 +0200

    update email templates

--- server/resources/views/register.blade.php
+++ server/resources/views/register.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
+
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,69 +14,90 @@
 		}
 	</style>
 	<style em="styles">
-.em-font-Inter-Regular {
-    font-weight: 400!important;
-    font-family: Inter,sans-serif!important;
-}
-.em-font-Mulish-Medium,.em-font-Mulish-Regular {
-    font-family: Mulish,sans-serif!important;
-}
-.em-font-Mulish-Regular {
-    font-weight: 400!important;
-}
-@media only screen and (max-device-width:640px),only screen and (max-width:640px) {
-    .em-mob-padding_right-0 {
-        padding-right: 0!important;
-    }
-    .em-mob-padding_left-0 {
-        padding-left: 0!important;
-    }
-    .em-mob-padding_top-0 {
-        padding-top: 0!important;
-    }
-    .em-mob-padding_top-10 {
-        padding-top: 10px!important;
-    }
-    .em-mob-font_size-12px {
-        font-size: 12px!important;
-    }
-    .em-narrow-table {
-        width: 100%!important;
-        max-width: 640px!important;
-        min-width: 320px!important;
-    }
-    .em-mob-padding_bottom-0 {
-        padding-bottom: 0!important;
-    }
-    .em-mob-padding_bottom-10 {
-        padding-bottom: 10px!important;
-    }
-    .em-mob-wrap {
-        display: block!important;
-    }
-    .em-mob-width-100perc {
-        width: 100%!important;
-        max-width: 100%!important;
-        min-width: 100%!important;
-    }
-    .em-mob-padding_right-20 {
-        padding-right: 20px!important;
-    }
-    .em-mob-padding_left-20 {
-        padding-left: 20px!important;
-    }
-    .em-mob-padding_bottom-20 {
-        padding-bottom: 20px!important;
-    }
-    .em-mob-padding_top-20 {
-        padding-top: 20px!important;
-    }
-    .em-mob-font_size-10px {
-        font-size: 10px!important;
-    }
-}
-</style>
-<script bis_use="true" type="text/javascript" charset="utf-8" data-bis-config="['facebook.com/','twitter.com/','youtube-nocookie.com/embed/','//vk.com/','//www.vk.com/','//linkedin.com/','//www.linkedin.com/','//instagram.com/','//www.instagram.com/','//www.google.com/recaptcha/api2/','//hangouts.google.com/webchat/','//www.google.com/calendar/','//www.google.com/maps/embed','spotify.com/','soundcloud.com/','//player.vimeo.com/','//disqus.com/','//tgwidget.com/','//js.driftt.com/','friends2follow.com','/widget','login','//video.bigmir.net/','blogger.com','//smartlock.google.com/','//keep.google.com/','/web.tolstoycomments.com/','moz-extension://','chrome-extension://','/auth/','//analytics.google.com/','adclarity.com','paddle.com/checkout','hcaptcha.com','recaptcha.net','2captcha.com','accounts.google.com','www.google.com/shopping/customerreviews','buy.tinypass.com','gstatic.com','secureir.ebaystatic.com','docs.google.com','contacts.google.com','github.com','mail.google.com','chat.google.com','audio.xpleer.com','keepa.com','static.xx.fbcdn.net','sas.selleramp.com','1plus1.video','console.googletagservices.com']" src="chrome-extension://eppiocemhmnlbhjplcgkofciiegomcon/../executers/vi-tr.js"></script></head>
+		.em-font-Inter-Regular {
+			font-weight: 400 !important;
+			font-family: Inter, sans-serif !important;
+		}
+
+		.em-font-Mulish-Medium,
+		.em-font-Mulish-Regular {
+			font-family: Mulish, sans-serif !important;
+		}
+
+		.em-font-Mulish-Regular {
+			font-weight: 400 !important;
+		}
+
+		@media only screen and (max-device-width:640px),
+		only screen and (max-width:640px) {
+			.em-mob-padding_right-0 {
+				padding-right: 0 !important;
+			}
+
+			.em-mob-padding_left-0 {
+				padding-left: 0 !important;
+			}
+
+			.em-mob-padding_top-0 {
+				padding-top: 0 !important;
+			}
+
+			.em-mob-padding_top-10 {
+				padding-top: 10px !important;
+			}
+
+			.em-mob-font_size-12px {
+				font-size: 12px !important;
+			}
+
+			.em-narrow-table {
+				width: 100% !important;
+				max-width: 640px !important;
+				min-width: 320px !important;
+			}
+
+			.em-mob-padding_bottom-0 {
+				padding-bottom: 0 !important;
+			}
+
+			.em-mob-padding_bottom-10 {
+				padding-bottom: 10px !important;
+			}
+
+			.em-mob-wrap {
+				display: block !important;
+			}
+
+			.em-mob-width-100perc {
+				width: 100% !important;
+				max-width: 100% !important;
+				min-width: 100% !important;
+			}
+
+			.em-mob-padding_right-20 {
+				padding-right: 20px !important;
+			}
+
+			.em-mob-padding_left-20 {
+				padding-left: 20px !important;
+			}
+
+			.em-mob-padding_bottom-20 {
+				padding-bottom: 20px !important;
+			}
+
+			.em-mob-padding_top-20 {
+				padding-top: 20px !important;
+			}
+
+			.em-mob-font_size-10px {
+				font-size: 10px !important;
+			}
+		}
+	</style>
+	<script bis_use="true" type="text/javascript" charset="utf-8" data-bis-config="['facebook.com/','twitter.com/','youtube-nocookie.com/embed/','//vk.com/','//www.vk.com/','//linkedin.com/','//www.linkedin.com/','//instagram.com/','//www.instagram.com/','//www.google.com/recaptcha/api2/','//hangouts.google.com/webchat/','//www.google.com/calendar/','//www.google.com/maps/embed','spotify.com/','soundcloud.com/','//player.vimeo.com/','//disqus.com/','//tgwidget.com/','//js.driftt.com/','friends2follow.com','/widget','login','//video.bigmir.net/','blogger.com','//smartlock.google.com/','//keep.google.com/','/web.tolstoycomments.com/','moz-extension://','chrome-extension://','/auth/','//analytics.google.com/','adclarity.com','paddle.com/checkout','hcaptcha.com','recaptcha.net','2captcha.com','accounts.google.com','www.google.com/shopping/customerreviews','buy.tinypass.com','gstatic.com','secureir.ebaystatic.com','docs.google.com','contacts.google.com','github.com','mail.google.com','chat.google.com','audio.xpleer.com','keepa.com','static.xx.fbcdn.net','sas.selleramp.com','1plus1.video','console.googletagservices.com']" src="chrome-extension://eppiocemhmnlbhjplcgkofciiegomcon/../executers/vi-tr.js"></script>
+</head>
+
 <body style="margin: 0px; padding: 0px; background-color: #ffffff;" bis_status="ok" bis_frame_id="125">
 	<span class="preheader" style="visibility: hidden; opacity: 0; color: #ffffff; height: 0px; width: 0px; font-size: 1px; display: none !important;">&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌</span>
 	<!--[if !mso]><!-->
@@ -87,7 +109,8 @@
 				<!--[if (gte mso 9)|(IE)]>
 				<table cellpadding="0" cellspacing="0" border="0" width="640"><tr><td>
 				<![endif]-->
-				<table cellpadding="0" cellspacing="0" width="100%" border="0" style="max-width: 640px; min-width: 640px; width: 640px;" class="em-narrow-table"><tr em="block" class="em-structure">
+				<table cellpadding="0" cellspacing="0" width="100%" border="0" style="max-width: 640px; min-width: 640px; width: 640px;" class="em-narrow-table">
+					<tr em="block" class="em-structure">
 						<td align="center" style="padding: 20px 40px; background-color: #ffffff;" class="em-mob-padding_top-10 em-mob-padding_right-20 em-mob-padding_bottom-10 em-mob-padding_left-20" bgcolor="#FFFFFF">
 							<table border="0" cellspacing="0" cellpadding="0" class="em-mob-width-100perc">
 								<tr>
@@ -95,7 +118,7 @@
 										<table cellpadding="0" cellspacing="0" border="0" width="100%" em="atom">
 											<tr>
 												<td align="left">
-													<a href="#" target="_blank"><img src="https://cdn.useblocks.io/26440/240403_1812_sOX1yPy.png" width="135" border="0" alt="" style="display: inline-block; width: 100%; max-width: 135px;"></a>
+													<a href="https://thedatinglab.es/" target="_blank"><img src="https://cdn.useblocks.io/26440/240403_1812_sOX1yPy.png" width="135" border="0" alt="" style="display: inline-block; width: 100%; max-width: 135px;"></a>
 												</td>
 											</tr>
 										</table>
@@ -103,7 +126,8 @@
 								</tr>
 							</table>
 						</td>
-					</tr><tr em="block" class="em-structure">
+					</tr>
+					<tr em="block" class="em-structure">
 						<td style="padding: 30px 40px; background-color: #df6761;" class="em-mob-padding_left-20 em-mob-padding_right-20 em-mob-padding_bottom-20 em-mob-padding_top-20" bgcolor="#DF6761" align="center">
 							<table border="0" cellspacing="0" cellpadding="0" class="em-mob-width-100perc">
 								<tr>
@@ -124,33 +148,61 @@
 											</tr>
 										</table>
 
-										
-										
+
+
 
 
 									</td>
 								</tr>
 							</table>
 						</td>
-					</tr><tr em="block" class="em-structure">
-  <td align="center" style="padding: 30px 40px; background-color: #ffffff; background-repeat: repeat;" class="em-mob-padding_left-20 em-mob-padding_right-20" bgcolor="#FFFFFF">
-    <table border="0" cellspacing="0" cellpadding="0" class="em-mob-width-100perc">
-      <tr>
-        <td width="560" valign="top" class="em-mob-wrap em-mob-width-100perc"><table cellpadding="0" cellspacing="0" border="0" width="100%" em="atom"><tr><td style="padding-bottom: 20px; background-color: #ffffff; background-repeat: repeat;" bgcolor="#FFFFFF">
-  <div style="font-family: Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; color: #595959;" class="em-font-Mulish-Regular em-mob-font_size-12px">Para comenzar a disfrutar de todas las funciones de nuestro sitio, te pedimos que completes dos pasos importantes:<span style="color: #ffffff;"></span></div>
-</td></tr></table><table cellpadding="0" cellspacing="0" border="0" width="100%" em="atom"><tr><td style="padding-bottom: 20px; background-color: #ffffff; background-repeat: repeat;" bgcolor="#FFFFFF">
-  <div style="font-family: Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; color: #595959;" class="em-font-Mulish-Regular em-mob-font_size-12px"><strong>Completa el&nbsp;test de&nbsp;compatibilidad: </strong>Este paso nos ayudará a&nbsp;entender tus intereses, preferencias y&nbsp;valores para conectarte con otros usuarios que sean afines a&nbsp;ti.<span style="color: #ffffff;"></span></div>
-</td></tr></table><table cellpadding="0" cellspacing="0" border="0" width="100%" em="atom"><tr><td style="padding-bottom: 20px; background-color: #ffffff; background-repeat: repeat;" bgcolor="#FFFFFF">
-  <div style="font-family: Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; color: #595959;" class="em-font-Mulish-Regular em-mob-font_size-12px"><strong>Completa tu Perfil: </strong>Proporciona información sobre ti y tus intereses. Cuanta más información incluyas, más fácil será para otros usuarios conocerte.<span style="color: #ffffff;">.</span></div>
-</td></tr></table><table cellpadding="0" cellspacing="0" border="0" width="100%" em="atom"><tr><td style="padding-bottom: 20px; background-color: #ffffff; background-repeat: repeat;" bgcolor="#FFFFFF">
-  <div style="font-family: Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; color: #595959;" class="em-font-Mulish-Regular em-mob-font_size-12px">¡Son los primeros pasos hacia encontrar a alguien especial!</div>
-</td></tr></table><table cellpadding="0" cellspacing="0" border="0" width="100%" em="atom"><tr><td style="padding-bottom: 10px; background-color: #ffffff; background-repeat: repeat;" bgcolor="#FFFFFF">
-  <div style="font-family: Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; color: #595959;" class="em-font-Mulish-Regular em-mob-font_size-12px">Con amor,<br>El equipo de Dating Lab,</div>
-</td></tr></table></td>
-      </tr>
-    </table>
-  </td>
-</tr><tr em="block" class="em-structure">
+					</tr>
+					<tr em="block" class="em-structure">
+						<td align="center" style="padding: 30px 40px; background-color: #ffffff; background-repeat: repeat;" class="em-mob-padding_left-20 em-mob-padding_right-20" bgcolor="#FFFFFF">
+							<table border="0" cellspacing="0" cellpadding="0" class="em-mob-width-100perc">
+								<tr>
+									<td width="560" valign="top" class="em-mob-wrap em-mob-width-100perc">
+										<table cellpadding="0" cellspacing="0" border="0" width="100%" em="atom">
+											<tr>
+												<td style="padding-bottom: 20px; background-color: #ffffff; background-repeat: repeat;" bgcolor="#FFFFFF">
+													<div style="font-family: Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; color: #595959;" class="em-font-Mulish-Regular em-mob-font_size-12px">Para comenzar a disfrutar de todas las funciones de nuestro sitio, te pedimos que completes dos pasos importantes:<span style="color: #ffffff;"></span></div>
+												</td>
+											</tr>
+										</table>
+										<table cellpadding="0" cellspacing="0" border="0" width="100%" em="atom">
+											<tr>
+												<td style="padding-bottom: 20px; background-color: #ffffff; background-repeat: repeat;" bgcolor="#FFFFFF">
+													<div style="font-family: Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; color: #595959;" class="em-font-Mulish-Regular em-mob-font_size-12px"><strong>Completa el&nbsp;test de&nbsp;compatibilidad: </strong>Este paso nos ayudará a&nbsp;entender tus intereses, preferencias y&nbsp;valores para conectarte con otros usuarios que sean afines a&nbsp;ti.<span style="color: #ffffff;"></span></div>
+												</td>
+											</tr>
+										</table>
+										<table cellpadding="0" cellspacing="0" border="0" width="100%" em="atom">
+											<tr>
+												<td style="padding-bottom: 20px; background-color: #ffffff; background-repeat: repeat;" bgcolor="#FFFFFF">
+													<div style="font-family: Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; color: #595959;" class="em-font-Mulish-Regular em-mob-font_size-12px"><strong>Completa tu Perfil: </strong>Proporciona información sobre ti y tus intereses. Cuanta más información incluyas, más fácil será para otros usuarios conocerte.<span style="color: #ffffff;">.</span></div>
+												</td>
+											</tr>
+										</table>
+										<table cellpadding="0" cellspacing="0" border="0" width="100%" em="atom">
+											<tr>
+												<td style="padding-bottom: 20px; background-color: #ffffff; background-repeat: repeat;" bgcolor="#FFFFFF">
+													<div style="font-family: Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; color: #595959;" class="em-font-Mulish-Regular em-mob-font_size-12px"><a style="color: inherit; font-weight:bold; text-decoration:none" href="https://thedatinglab.es/" target="_blank">¡Haz clic en este enlace</a> y da el primer paso hacia encontrar a alguien especial!</div>
+												</td>
+											</tr>
+										</table>
+										<table cellpadding="0" cellspacing="0" border="0" width="100%" em="atom">
+											<tr>
+												<td style="padding-bottom: 10px; background-color: #ffffff; background-repeat: repeat;" bgcolor="#FFFFFF">
+													<div style="font-family: Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; color: #595959;" class="em-font-Mulish-Regular em-mob-font_size-12px">Con amor,<br>El equipo de Dating Lab,</div>
+												</td>
+											</tr>
+										</table>
+									</td>
+								</tr>
+							</table>
+						</td>
+					</tr>
+					<tr em="block" class="em-structure">
 						<td align="center" style="padding: 32px 40px; background-color: #df6761;" class="em-mob-padding_left-20 em-mob-padding_right-20 em-mob-padding_top-20 em-mob-padding_bottom-20" bgcolor="#DF6761">
 							<table border="0" cellspacing="0" cellpadding="0" class="em-mob-width-100perc">
 								<tr>
@@ -159,13 +211,14 @@
 											<tr>
 												<td align="center" style="padding-bottom: 16px; height: 12px;" height="12">
 													<table border="0" cellspacing="0" cellpadding="0">
-														<tr><td style="padding: 0px 8px;" em="atom">
-																<a href="https://www.tiktok.com/" target="_blank"><img src="https://cdn.useblocks.io/26440/240403_1812_UBUEHoI.png" border="0" alt="Tik Tok" style="display: block; width: 100%; max-width: 14px;" width="14"></a>
-															</td><td style="padding: 0px 8px;" em="atom">
-																<a href="https://www.linkedin.com/company/datinglab/about/" target="_blank"><img src="https://cdn.useblocks.io/26440/240403_1812_tAwBfaU.png" border="0" alt="Linkedin" style="display: block; width: 100%; max-width: 14px;" width="14"></a>
-															</td><td style="padding: 0 8px;" em="atom">
+														<tr>
+															<td style="padding: 0px 8px;" em="atom">
+																<a href="https://www.linkedin.com/company/datinglab/" target="_blank"><img src="https://cdn.useblocks.io/26440/240403_1812_tAwBfaU.png" border="0" alt="Linkedin" style="display: block; width: 100%; max-width: 14px;" width="14"></a>
+															</td>
+															<td style="padding: 0 8px;" em="atom">
 																<a href="https://www.instagram.com/the.dating.lab/" target="_blank"><img src="https://cdn.useblocks.io/26440/240403_1812_cd0zHfA.png" border="0" alt="Instagram" style="display: block; width: 100%; max-width: 14px;" width="14"></a>
-															</td></tr>
+															</td>
+														</tr>
 													</table>
 												</td>
 											</tr>
@@ -173,16 +226,16 @@
 										<table cellpadding="0" cellspacing="0" border="0" width="100%" em="atom">
 											<tr>
 												<td>
-													<div class="em-font-Mulish-Regular em-mob-font_size-10px" style="font-family: -apple-system, 'Segoe UI', 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif; font-size: 12px; line-height: 20px; color: #ffffff;" align="center">WhatsApp: +34  669 945 836<br>Email: info@thedatinglab.es</div>
+													<div class="em-font-Mulish-Regular em-mob-font_size-10px" style="font-family: -apple-system, 'Segoe UI', 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif; font-size: 12px; line-height: 20px; color: #ffffff;" align="center">Email: datinglab.experiences@gmail.com<br>WhatsApp: +34 669 945 836<br> <a style="color: inherit; text-decoration:none" href="https://thedatinglab.es/" target="_blank">thedatinglab.es</a> </div>
 												</td>
 											</tr>
 										</table>
-										
 									</td>
 								</tr>
 							</table>
 						</td>
-					</tr></table>
+					</tr>
+				</table>
 				<!--[if (gte mso 9)|(IE)]>
 				</td></tr></table>
 				<![endif]-->
@@ -190,4 +243,5 @@
 		</tr>
 	</table>
 </body>
+
 </html>
